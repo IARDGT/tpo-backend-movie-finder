f154d5c8afe26f3a94ae31d1510d6e7ddcbb6150
jeremias mazzetti <jere.mazzetti@gmail.com>
2024-05-05 03:39:04 -0300

    se cambia nombre de proyecto para poder correrlo.

--- android/app/src/main/java/com/tpo-frontend-movie-finder/MainActivity.kt
+++ android/app/src/main/java/com/tpo-frontend-movie-finder/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "front"
+  override fun getMainComponentName(): String = "tpo-frontend-movie-finder"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
